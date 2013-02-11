a8b4ec66b7e3742ff5654ecec433a1e9a94041e4
jmgr2007 <michealmyersreal@gmail.com>
2013-02-10 19:49:47 -0600

    Added perms to /pl command

--- src/me/jmgr2007/Reloader/Pl.java
+++ src/me/jmgr2007/Reloader/Pl.java
@@ -9,8 +9,12 @@ public class Pl implements Listener {
 	@EventHandler
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
 		if(event.getMessage().equalsIgnoreCase("/pl") || event.getMessage().equalsIgnoreCase("/plugins")) {
-			if(Bukkit.getPluginManager().getPlugin("Reloader") != null && Bukkit.getServer().getPluginManager().getPlugin("Reloader").getConfig().getBoolean("pl", true)) {
+			if(Bukkit.getPluginManager().getPlugin("Reloader") != null && Bukkit.getServer().getPluginManager().getPlugin("Reloader").getConfig().getBoolean("pl", true) && event.getPlayer().hasPermission("reloader.list")) {
 				Utils.list(event.getPlayer());
+				event.setCancelled(true);
+			}
+			if(Bukkit.getPluginManager().getPlugin("Reloader") != null && Bukkit.getServer().getPluginManager().getPlugin("Reloader").getConfig().getBoolean("default-pl", false)) {
+				event.getPlayer().sendMessage("§cYou can not use this command on this server");
 				event.setCancelled(true);
 			}
 		}
